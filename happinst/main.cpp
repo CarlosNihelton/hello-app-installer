713b30a9cf38c8fb897c05a3da78ab7ad4e33521
Carlos Nihelton <cnihelton@ubuntu.com>
2025-07-04 15:47:36 -0300

    Boilerplate C++/WinRT packaged solution
    For now just a hello world in C++,
    but scaffolded to use WinRT and
    to be MSIX-packaged.
    
    Works but no auto updates.

---
+++ happinst/main.cpp
@@ -0,0 +1,13 @@
+﻿#include "pch.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+
+std::string versionString() noexcept;
+
+int main()
+{
+    winrt::init_apartment();
+
+    std::cout << "Hello World, PoC Version: ";
+}
