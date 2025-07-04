f10a8451678794fd441d5a60cc3ef8805c724d88
Carlos Nihelton <cnihelton@ubuntu.com>
2025-07-04 15:48:24 -0300

    Implements the app functionality
    Print the MSIX package version :)

--- happinst/main.cpp
+++ happinst/main.cpp
@@ -9,5 +9,23 @@ int main()
 {
     winrt::init_apartment();
 
-    std::cout << "Hello World, PoC Version: ";
+    std::cout << "Hello World, PoC Version: " << versionString() << std::endl;
+    pause();
+    return 0;
+}
+
+std::string versionString() noexcept try {
+    winrt::Windows::ApplicationModel::Package package = winrt::Windows::ApplicationModel::Package::Current();
+    winrt::Windows::ApplicationModel::PackageId packageId = package.Id();
+
+    // Gets the MSIX package full version.
+    winrt::Windows::ApplicationModel::PackageVersion version = packageId.Version();
+
+    return std::format("{}.{}.{}.{}", version.Major, version.Minor, version.Build,version.Revision);
+}
+catch (winrt::hresult_error const& err) {
+    return std::format("<not versioned> ({})", winrt::to_string(err.message()));
+}
+catch (...) {
+    return "<UNHANDLED EXCEPTION>";
 }
--- happinst/pch.h
+++ happinst/pch.h
@@ -1,3 +1,8 @@
 ﻿#pragma once
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.ApplicationModel.h>
+#include <format>
+#include <iostream>
+#include <conio.h>
+#define pause _getch
